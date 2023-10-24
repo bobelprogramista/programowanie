1beb39db91a5ef85b557fd5011dd85691c8f7646
bobel69420 <brunoolewski807@gmail.com>
2023-10-24 14:59:02 +0200

    Komentarz

---
+++ aaa.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	short zmienna = 2;
+	cout << "a";
+	cout << endl;
+	cout << "wszystko wina dawida";
+	cout << endl;
+	cout << "liczba: " << zmienna;
+	
+	return 0;
+}
