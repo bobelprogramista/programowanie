858a13a5ba9a8b62543eb17ac60250872b0b8cbc
bobel69420 <brunoolewski807@gmail.com>
2023-10-25 14:49:01 +0200

    Komentarz

--- aaa.cpp
+++ aaa.cpp
@@ -4,12 +4,36 @@ using namespace std;
 
 int main(){
 
+	// deklaracja zmiennych
 	short zmienna = 2;
+	float zmiennoprzecinkowa = 3.21;
+	string tekst = "tekst";
+	char znak = 'A';
+	bool logiczny = false;
+	
 	cout << "a";
 	cout << endl;
 	cout << "wszystko wina dawida";
+	cout << endl; // koniec lini
+	cout << "liczba: " << zmienna; //wyswietlenie zmiennych
+	
+	string wyraz;
 	cout << endl;
-	cout << "liczba: " << zmienna;
+	cout << "wpisz cos: ";
+	cin >> wyraz;
+	cout << "wpisales " << wyraz;
+	
+	// imie i wiek
+	cout << endl;
+	string imie;
+	float wiek;
+	cout << "wpisz imie ";
+	cin >> imie;
+	cout << endl;
+	cout << "wpisz wiek ";
+	cin >> wiek;
+	cout << endl;
+	cout << "nazywasz sie " << imie << " i masz " << wiek << " lat";
 	
 	return 0;
 }
