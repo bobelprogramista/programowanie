79c570aa1e362acabd96a1ee8c266f61fd973f0f
bobel69420 <76212682+bobelprogramista@users.noreply.github.com>
2024-04-28 20:52:28 +0200

    Add files via upload

---
+++ czynniki pierwsze.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int liczba;
+
+	cout<<"wpisz liczbe: ";
+	cin>>liczba;
+
+	cout<<endl;
+
+	cout << "czynniki pierwsze: " << endl;
+	for (int b = 2; liczba > 1; ++b) {
+		while (liczba % b == 0) {
+		cout << b << " ";
+		liczba /= b;
+		}
+	}
+	
+}
