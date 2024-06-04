833ca49dfc5b4d7897734016cdbadfd4bb9894a9
bobel69420 <76212682+bobelprogramista@users.noreply.github.com>
2024-06-04 11:38:33 +0200

    Create algorytm-wyznaczania-liczb-naturalnych.cpp

---
+++ algorytm-wyznaczania-liczb-naturalnych.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void generujLiczbyPierwsze(int iloscLiczbPierwszych) {
+    int znalezioneLiczby = 0;
+    int liczba = 2;
+
+    cout << "Generowanie " << iloscLiczbPierwszych << " pierwszych liczb pierwszych:" << endl;
+
+    while (znalezioneLiczby < iloscLiczbPierwszych) {
+        bool jestPierwsza = true;
+
+        for (int dzielnik = 2; dzielnik <= liczba / 2; ++dzielnik) {
+            if (liczba % dzielnik == 0) {
+                jestPierwsza = false;
+                break;
+            }
+        }
+
+        if (jestPierwsza) {
+            cout << "Znaleziono liczbe: " << liczba << endl;
+            znalezioneLiczby++;
+        }
+        liczba++;
+    }
+
+    cout << "Wygenerowano " << iloscLiczbPierwszych << " liczb." << endl;
+}
+
+int main() {
+    int n;
+    cout << "Podaj ilosc liczb pierwszych do wygenerowania: ";
+    cin >> n;
+    generujLiczbyPierwsze(n);
+    return 0;
+}
